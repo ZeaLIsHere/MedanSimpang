7cca8926396385cb19a0c6e3f6d741ee6a63826e
ZeaLIsHere <tariq.rahmadari23@gmail.com>
2026-06-26 20:06:07 +0700

    feat(map): switch tileLayer to CartoDB Voyager and enable mouse scroll/pinch zoom

--- src/components/map/MedanMap.tsx
+++ src/components/map/MedanMap.tsx
@@ -114,11 +114,15 @@ export default function MedanMap({
 
     if (!mapRef.current) {
       mapRef.current = L.map(mapContainerRef.current, {
-        scrollWheelZoom: false,
+        scrollWheelZoom: true,
+        touchZoom: true,
+        dragging: true,
       }).setView([centerLat, centerLng], zoom);
 
-      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+        subdomains: 'abcd',
+        maxZoom: 20
       }).addTo(mapRef.current);
     }
 
